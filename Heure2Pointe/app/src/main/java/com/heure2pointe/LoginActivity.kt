29ee9d3c9198fdc95a842e71858f9d74ea8b6d79
MakiiX <t.bagnolati@gmail.com>
2020-10-14 14:18:22 +0200

    Correction d'organisation des activités et layouts

--- Heure2Pointe/app/src/main/java/com/heure2pointe/LoginActivity.kt
+++ Heure2Pointe/app/src/main/java/com/heure2pointe/LoginActivity.kt
@@ -1,22 +1,50 @@
 package com.heure2pointe
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
+
+val urlAdel = "https://adel.adrar-formation.eu/login/index.php"
+
+val username = "your_username"
+val password = "your_password"
+
+val idUsername = "username"
+val idPassword = "password"
+val idButton = "loginbtn"
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        webview.webViewClient = WebViewClient()
+        webview.settings.javaScriptEnabled = true
+
+        webview.loadUrl(urlAdel)
+        webview.setWebViewClient(object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                view.loadUrl(url)
+                return true
+            }
+
+            override fun onPageFinished(view: WebView, url: String) {
+                autoFillAndClick(idUsername, idPassword, idButton, username, password)
+                //TODO Valider message
+            }
+        })
     }
 
+    fun autoFillAndClick(idUsername: String, idPassword: String, idButton: String, username: String, password: String) {
+        //Auto-fill Text
+        webview.loadUrl("javascript:(function() { document.getElementById('$idUsername').value = '$username'; ;})()")
+        webview.loadUrl("javascript:(function() { document.getElementById('$idPassword').value = '$password'; ;})()")
 
-    //                                                           Temporaire !!!
-    fun onClickNextButton(view: View) {
-        startActivity(Intent(this, WebViewActivity::class.java))
+        //Auto click Button
+        webview.loadUrl("javascript:(function() { document.getElementById('$idButton').click(); })()")
+
+        println("AUTOFILL")
     }
-    //                                                           Temporaire !!!
-
-
 }
