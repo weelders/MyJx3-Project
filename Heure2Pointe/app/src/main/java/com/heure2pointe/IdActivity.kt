302b378b34b0dc1d5f460f73e17ae1f0e1113c28
Amandine G <57950023+Astheis@users.noreply.github.com>
2020-10-14 14:10:28 +0200

    changement de nom

--- Heure2Pointe/app/src/main/java/com/heure2pointe/IdActivity.kt
+++ Heure2Pointe/app/src/main/java/com/heure2pointe/IdActivity.kt
@@ -3,9 +3,9 @@ package com.heure2pointe
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class LoginActivity : AppCompatActivity() {
+class IdActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_id)
     }
 }
