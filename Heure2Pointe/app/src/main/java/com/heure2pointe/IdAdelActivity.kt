f5c2cb02f1dc42fcf6865c0508477c6b0233b92a
Amandine G <57950023+Astheis@users.noreply.github.com>
2020-10-14 14:33:54 +0200

    Ajout id_region

--- Heure2Pointe/app/src/main/java/com/heure2pointe/IdAdelActivity.kt
+++ Heure2Pointe/app/src/main/java/com/heure2pointe/IdAdelActivity.kt
@@ -3,9 +3,9 @@ package com.heure2pointe
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class IdActivity : AppCompatActivity() {
+class IdAdelActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_id)
+        setContentView(R.layout.activity_id_adel)
     }
 }
