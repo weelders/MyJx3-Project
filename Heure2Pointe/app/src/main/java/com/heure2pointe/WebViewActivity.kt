09561ea946f21b056b666c3bebc54b092cdb2e47
MakiiX <t.bagnolati@gmail.com>
2020-10-14 09:55:38 +0200

    Création de l'activité : WebViewActivity et création d'un layout temporaire

---
+++ Heure2Pointe/app/src/main/java/com/heure2pointe/WebViewActivity.kt
@@ -0,0 +1,13 @@
+package com.heure2pointe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WebViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web_view)
+
+
+    }
+}
