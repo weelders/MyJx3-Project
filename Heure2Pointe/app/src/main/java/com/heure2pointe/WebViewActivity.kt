807e3651e9cc32bf3f35daba9356239ca6d274f6
MakiiX <t.bagnolati@gmail.com>
2020-10-14 10:15:54 +0200

    WebView de WebViewActivity fonctionnel

--- Heure2Pointe/app/src/main/java/com/heure2pointe/WebViewActivity.kt
+++ Heure2Pointe/app/src/main/java/com/heure2pointe/WebViewActivity.kt
@@ -2,11 +2,45 @@ package com.heure2pointe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebViewClient
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_web_view.*
+import kotlin.concurrent.thread
 
 class WebViewActivity : AppCompatActivity() {
+
+    var myThread: Thread? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web_view)
+
+
+        webview.webViewClient = WebViewClient()
+        webview.settings.javaScriptEnabled = true
+
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // ONCLICK
+    ///////////////////////////////////////////////////////////////////////////
+
+    //onClick
+    fun onClickSearchWeb(view: View) {
+
+        val url = et_url.text.toString()
+
+        if (myThread == null || myThread?.state == Thread.State.TERMINATED) {
+            myThread = thread {
+                runOnUiThread {
+                    webview.loadUrl(url)
+                }
+            }
+        } else {
+            Toast.makeText(this, "Chargement déjà en cours", Toast.LENGTH_SHORT).show()
+        }
 
 
     }
