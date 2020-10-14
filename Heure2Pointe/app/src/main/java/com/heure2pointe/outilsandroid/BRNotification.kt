014bb3b15b04fe4d3d36c749963b568a9f09af72
nb12nb12 <61192595+nb12nb12@users.noreply.github.com>
2020-10-14 11:52:16 +0200

    Add Notifications

---
+++ Heure2Pointe/app/src/main/java/com/heure2pointe/outilsandroid/BRNotification.kt
@@ -0,0 +1,23 @@
+package com.heure2pointe.outilsandroid
+
+import android.app.Notification
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationManagerCompat
+
+class NotificationPublisher : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        //Log.w("TAG_", "BRNotification")
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        //TODO("MyBroadcastReceiverNotification.onReceive() is not implemented")
+        //On récupère la notification reçu
+        //on l'affiche
+        intent.getParcelableExtra<Notification>("MaCle")?.let {
+            //on l'affiche
+            val ncm = NotificationManagerCompat.from(context)
+            ncm.notify(28, it)
+        }
+    }
+}
