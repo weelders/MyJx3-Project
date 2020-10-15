91995f576d19e185d82f28aae95c6a6f3470cd73
Maxime Mourey <maxime.moure31@gmail.com>
2020-10-15 10:27:34 +0200

    UX MAJ activity final disign

--- Heure2Pointe/app/src/main/java/com/heure2pointe/IdAdelActivity.kt
+++ Heure2Pointe/app/src/main/java/com/heure2pointe/IdAdelActivity.kt
@@ -1,7 +1,7 @@
 package com.heure2pointe
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class IdAdelActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
