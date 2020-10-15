4dbbb15a0af2e01fb39a56cd066bd407cca21d1d
MakiiX <t.bagnolati@gmail.com>
2020-10-15 11:13:54 +0200

    Création du fonctionnement (enregistrement et gérer les activities des IDActivities

--- Heure2Pointe/app/src/main/java/com/heure2pointe/IdAdelActivity.kt
+++ Heure2Pointe/app/src/main/java/com/heure2pointe/IdAdelActivity.kt
@@ -1,29 +1,42 @@
 package com.heure2pointe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_id_adel.*
 
 class IdAdelActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_id_adel)
 
-        //TODO
-        val sp = getSharedPreferences("dataLogs", 0)
-
-        val edt = sp.edit()
-
-        edt.putString("usernameRegion","")
-        edt.putString("passwordRegion","")
-
-<<<<<<< HEAD
-        edt.commit()
     }
-    //TODO
-    fun onClickBtnIdAdel(view: View) {}
+
+    //Onclick -> enregistre les données et passe à la page suivante
+    fun onClickBtnIdAdel(view: View) {
+
+        registData()
+        startActivity(Intent(this,LoginActivity::class.java))
+
+        finish()
+    }
+
+    fun registData() {
+        //Stockage Preferences
+        val gsp = getSharedPreferences("dataLogs", 0)
+        val edt = gsp.edit()
+
+        //Récup des données
+        val nameAdel = et_name_adel.text.toString()
+        val pswAdel = et_pwd_adel.text.toString()
+
+        //Enregistre les données
+        edt.putString("usernameAdel", nameAdel)
+        edt.putString("passwordAdel", pswAdel)
+        edt.putBoolean("isAlredyLog", true)
+
+        //Apply
+        edt.apply()
+    }
 }
-=======
-
-
->>>>>>> 7568b1083cb4486aa6a31bf44cf898a77bc43e17
