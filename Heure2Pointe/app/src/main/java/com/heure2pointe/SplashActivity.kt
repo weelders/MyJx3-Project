01ba21ddeee257f6ca7caffd5992a140b159d19a
Amandine G <57950023+Astheis@users.noreply.github.com>
2020-10-15 13:40:14 +0200

    Maj bt start splash

--- Heure2Pointe/app/src/main/java/com/heure2pointe/SplashActivity.kt
+++ Heure2Pointe/app/src/main/java/com/heure2pointe/SplashActivity.kt
@@ -1,11 +1,22 @@
 package com.heure2pointe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+    }
+
+    //Onclick -> enregistre les données et passe à la page suivante
+    fun onClick(view: View) {
+
+        registData()
+        startActivity(Intent(this, LoginActivity::class.java))
+
+        finish()
     }
 }
